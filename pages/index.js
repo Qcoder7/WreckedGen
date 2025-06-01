3cc4c99de662ec51b1a40dc6c52f78c16538a2a3
Qcoder7 <qcoder.wrecked@outlook.com>
2025-06-01 11:03:17 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      height: '100vh',
+      backgroundColor: '#000',
+      color: 'white',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <p>Home Page — Coming Soon</p>
+    </div>
+  );
+}
