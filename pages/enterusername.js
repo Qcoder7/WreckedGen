619992508ce65b8a44ea2b7e696b0ecd85282003
Qcoder7 <qcoder.wrecked@outlook.com>
2025-06-01 11:03:01 +0530

    Create enterusername.js

---
+++ pages/enterusername.js
@@ -0,0 +1,17 @@
+export default function EnterUsername() {
+  return (
+    <div style={{
+      background: 'linear-gradient(135deg, #1ca7ec, #1f2f98)',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'white',
+      fontSize: '1.8rem',
+      textAlign: 'center',
+      padding: '0 20px'
+    }}>
+      <p>Please Wait While We Verify You And Generate Your Link</p>
+    </div>
+  );
+}
