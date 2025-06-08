3e9dcf369056252432fcfff50191f79eb0c43674
Qcoder7 <qcoder.wrecked@outlook.com>
2025-06-08 15:01:23 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{
-      height: '100vh', // this line is crucial
-      backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.95)), url('/background.jpg')",
+      minHeight: '100vh',
+      backgroundImage: "linear-gradient(rgba(0,0,0,0.85), rgba(0,0,0,0.95)), url('/background.jpg')",
       backgroundColor: 'black',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
