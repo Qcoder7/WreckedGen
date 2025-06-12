4a452eb9c1e6d7fda5b7c36916dd954cf2b9bfc9
Qcoder7 <qcoder.wrecked@outlook.com>
2025-06-12 16:09:05 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+export default function Home() {
+  return (
 <div>
   <meta charSet="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -324,3 +326,5 @@
     ================================================== */} 
   {/* Placed at the end of the document so the pages load faster */} 
 </div>
+    }
+)
