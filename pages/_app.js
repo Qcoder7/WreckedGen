b520ffef92ed53577eef495f22c9e0255c3f631f
Qcoder7 <qcoder.wrecked@outlook.com>
2025-06-12 17:45:18 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 // pages/_app.js
 import '../styles/styles.css'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Roboto&family=Poppins:wght@200;300;400;500;600;700&display=swap" 
+          rel="stylesheet" 
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
