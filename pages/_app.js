964ac507d0c39bbcd801c764d283e4db7a8e62b7
Qcoder7 <qcoder.wrecked@outlook.com>
2025-06-13 08:06:35 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import { useEffect } from 'react';
+
 // pages/_app.js
 import '../styles/styles.css'
 import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: 'ease-in-out',
+      once: true,
+    });
+  }, []);
+
   return (
     <>
       <Head>
