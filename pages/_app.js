aeda29b13999c701bea4bf96669f7e391035df2d
Qcoder7 <qcoder.wrecked@outlook.com>
2025-06-14 20:34:22 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,18 @@
-import 'aos/dist/aos.css';
-import AOS from 'aos';
-import { useEffect } from 'react';
-
 // pages/_app.js
-import '../styles/styles.css'
-import Head from 'next/head'
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { queryClient } from "../client/src/lib/queryClient";
 
-export default function MyApp({ Component, pageProps }) {
-
-  useEffect(() => {
-    AOS.init({
-      duration: 1200,
-      easing: 'ease-out-cubic',
-      once: false,
-      mirror: true,
-      offset: 100,
-      delay: 100,
-    });
-    
-    // Refresh AOS on route changes
-    AOS.refresh();
-  }, []);
-
+function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Roboto&family=Poppins:wght@200;300;400;500;600;700;800&display=swap" 
-          rel="stylesheet" 
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Wrecked G3N - Premium Services, Tools, and Accounts" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Component {...pageProps} />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
