e5a35f11f85e39a464951ded05ff91b06e88af25
Qcoder7 <qcoder.wrecked@outlook.com>
2025-06-14 20:31:56 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,2 +1,16 @@
 // pages/index.js
-export { default } from '../client/src/pages/Home';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "../client/src/pages/Home";
+
+export default function IndexPage() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Home />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
